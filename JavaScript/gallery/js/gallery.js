0591e73662163176faf237fc4b99feea6a9557dc
Bob Morrissey <rmorriss@illinois.edu>
2017-01-24 21:46:53 -0600

    completed the javaScript gallery code

--- JavaScript/gallery/js/gallery.js
+++ JavaScript/gallery/js/gallery.js
@@ -8,10 +8,17 @@ function upDate(previewPic){
     2) Change the text  of the div with the id = "image" 
     to the alt text of the preview image 
     */
+    document.getElementById('image').style =' background-image: url("'+previewPic.src+'")';
+
+    document.getElementById('image').innerHTML = previewPic.alt
   
 	}
 
 	function unDo(){
+    
+    document.getElementById('image').style = 'background-image: url("")'    
+
+    document.getElementById('image').innerHTML = 'Hover over an image below to display here.'
      /* In this function you should 
     1) Update the url for the background image of the div with the id = "image" 
     back to the orginal-image.  You can use the css code to see what that original URL was
